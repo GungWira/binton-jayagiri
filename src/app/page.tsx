66541f9c3a4659dedfc7335f815be6888fbc63ba
GungWira <wiradarma002@gmail.com>
2024-05-03 12:11:32 +0800

    updatee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,28 @@ import Loading from "@/components/loading";
 import { cookies } from 'next/headers'
 import Login from "@/components/login";
 
+
+const fetchUpdate = async () =>{
+  const update = await fetch(`${process.env.NEXT_PUBLIC_REQ_URL}/update`,{
+    method : "GET",
+    cache : "no-store"
+  })
+  return update.ok
+} 
+
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   const cookieStrore = cookies()
   const name = cookieStrore.get('name')
   const id = cookieStrore.get('id')
-  const update = await fetch(`${process.env.NEXT_PUBLIC_REQ_URL}/update`,{
-    method : "GET",
-    cache : "no-store"
-  })
-  console.log(update)
+  const serve = await fetchUpdate()
+  console.log(fetchUpdate)
 
   return (
     <>
       <main>
-        <Loading serve={update.ok}/>
+        <Loading serve={serve}/>
         {name === undefined && id === undefined ? 
         <Login/>
         :
